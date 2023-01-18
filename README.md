5d10c6b4b6355c15fcb27658d1728c5285ba0569
aasem-research-work <aasem.research.work@gmail.com>
2023-01-18 20:36:23 +0500

    update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # boxed-ml-boilerplate
+
 Deploy you machine learning tasks (train, predict, etc)
 
 ## create environment
@@ -10,3 +11,13 @@ pip install -r requirements.txt
 ```
 
 PS: Use ```pip freeze > requirements.txt``` when new packages are installed
+
+## Deploy/Run
+
+```sh
+FLASK_APP=app.py FLASK_DEBUG=1 TEMPLATES_AUTO_RELOAD=1 flask run
+```  
+or  
+```sh
+flask run
+```
--- ml_module.py
+++ ml_module.py
@@ -2,7 +2,7 @@ import sys, getopt
 import os,json
 import pandas as pd
 import numpy as np
-#from deepface import DeepFace 
+#import tensorflow as tf
 
 class ML_Module:
     def __init__(self, p1, p2):
@@ -24,29 +24,9 @@ class ML_Module:
         return payload
 
 
-def recognize (path_to_imagefile):
-    with open('data_deepface.json', 'r') as f:
-        dic_pre_computed_embedings=json.load(f)
-
-    embeddings = DeepFace.represent(img_path = path_to_imagefile, model_name = 'Facenet512',enforce_detection=False)
-    df_score = pd.DataFrame(columns=['name','score'])
-    for name in dic_pre_computed_embedings:
-        aa=dic_pre_computed_embedings[name]
-        for inst in range (len(aa)):
-            pre_computed=aa[inst][1]
-            score=DeepFace.dst.findCosineDistance(embeddings, pre_computed)
-            df_score_row = pd.DataFrame([[name,score]], columns=['name','score'])
-            df_score=pd.concat([ df_score,df_score_row])
-            df_final=df_score.sort_values('score').head(3)
-    #print (df_final) 
-    #payload=df_final.reset_index().to_json()
-    response_json=df_final.to_dict('list')
-    return response_json
-
-
 def main(argv):
    inputfile = ''
-   outputfile = ''
+
    opts, args = getopt.getopt(argv,"m:i:",["mode=","ifile="])
    for opt, arg in opts:
       if opt in ("-i", "--ifile"):
@@ -61,7 +41,6 @@ def main(argv):
          ml=ML_Module(p1=1,p2=2)
          ml.load_model()
          ml.predict(ifile)
-         #print (recognize(ifile))
 
 
 if __name__ == "__main__":
